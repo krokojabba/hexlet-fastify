b47b4015bda5127eda89eea68ed5dfd219dd5fc5
krokojabba <krokojabba@gmail.com>
2024-08-19 11:14:45 +0300

    add points:   /users   /hello

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/users', (req, res) => {
+  res.send('GET /users');
+});
+
+app.post('/users', (req, res) => {
+  res.send('POST /users');
+});
+
+app.get('/hello', (req, res) => {
+  const name = req.query.name ?? 'World'
+  res.send(`Hello, ${name}!`);
+});
+
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
